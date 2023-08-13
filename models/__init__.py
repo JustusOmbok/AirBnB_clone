93e42bffc4e3e185639c6d400c6369fd32d795eb
JustusOmbok <ombokjustus@gmail.com>
2023-08-12 23:25:14 -0700

    added hasattr

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,5 @@ from models.engine.file_storage import FileStorage
 """
 This file makes python to treate diretories as packages
 """
-if __name__!= "__main__":
-    storage = FileStorage()
-    storage.reload()
+storage = FileStorage()
+storage.reload()
